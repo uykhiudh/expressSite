60e0df1019227fbace7ccedbcd99c2e63c89fcbc
345654443@qq.com <345654443@qq.com>
2019-10-21 18:12:59 +0800

    提交9

--- meadowlark.js
+++ meadowlark.js
@@ -16,6 +16,7 @@ var handlebars = require('express-handlebars')
         /*,extname:'.hbs'*/
     });
 
+
 // 文件上传需要
 var formidable = require('formidable');
 
@@ -28,10 +29,22 @@ app.set('view engine','handlebars');
 
 app.set('port', process.env.RORT || 3000);
 
+// cookie 和 session
+var credentials = require('./credential');
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')());
+
 app.use(express.static(__dirname + '/public'));
 
 app.use(function (req,res,next){
     res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
+app.use(function (req, res, next) {
+    // 如果有即显消息，把它传到上下文中，然后清除它
+    res.locals.flash = req.session.flash;
+    delete  req.session.flash;
     next();
 });
 
@@ -84,6 +97,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.favicon(__dirname+'/public/img/favicon.ico'));
 
 app.get('/',function (req, res) {
+    res.cookie('monster','nom nom');
+    res.cookie('signed_monster','nom nom',{signed:true});
     res.render('home');
 });
 
@@ -92,6 +107,47 @@ app.get('/newsletter',function (req, res) {
         csrf:'CSRF token goes here'
     });
 });
+
+// 即显消息
+app.post('/newsletter',function (req, res) {
+    var name = req.body.name || '',
+        email = req.body.email || '';
+    // 输入验证
+    if (!email.match(VALID_EMAIL_REGEX)) {
+        if(req.xhr){
+            return res.json({error: 'Invalid name email address.'});
+        }
+        req.session.flash = {
+            type: 'danger',
+            intro: 'Validation error!',
+            message: 'The email address you entered was not valid.'
+        };
+        return res.redirect(303,'/newsletter/archive');
+    }
+    new NewsletterSignup({name: name, email: email}).save(function (err) {
+        if(err){
+            if(req.xhr){
+                return res.json({error: 'Database error.'});
+            }
+            req.session.flash = {
+                type: 'danger',
+                intro: 'Database error!',
+                message: 'There was a database error; please try again later.'
+            };
+            return res.redirect(303, '/newsletter/archive');
+        }
+        if(req.xhr){
+            return res.json({success: true});
+        }
+        req.session.flash = {
+            type: 'success',
+            intro: 'Thank you!',
+            message: 'You have now been signed up for the newsletter.'
+        };
+        return res.redirect(303, '/newsletter/archive');
+    });
+});
+
 
 app.post('/process',function (req, res) {
     // console.log('Form (from queryString): ' + req.query.form);
@@ -137,6 +193,7 @@ app.post('/contest/vacation-photo/:year/:month',function (req, res) {
 
 
 app.get('/about',function (req, res) {
+    res.clearCookie('monster');
     res.render('about',{
         fortune:fortune.getFortune(),
         pageTestScript:'/qa/tests-about.js'
